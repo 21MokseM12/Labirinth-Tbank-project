b841b9229b7afd83fb3449936679dc4a1132a8e8
moksem <maksim.buynov.00@mail.ru>
2024-10-11 15:23:42 +0300

    feature: добавлены названия алгоритмов генерации лабиринтов

--- src/main/java/backend/academy/labyrinths/enums/GeneratorType.java
+++ src/main/java/backend/academy/labyrinths/enums/GeneratorType.java
@@ -1,5 +1,16 @@
 package backend.academy.labyrinths.enums;
 
 public enum GeneratorType {
-    ELLER_GENERATOR
+    ELLER_GENERATOR("Алгоритм Эллера"), DFS("DFS");
+
+    private final String name;
+
+    GeneratorType(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
