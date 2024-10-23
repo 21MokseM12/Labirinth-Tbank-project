808408e9ad281f014cd11c1ca781f66243920e06
moksem <maksim.buynov.00@mail.ru>
2024-10-23 11:53:18 +0300

    feature: добавлено поле цены клетки, переопределены методы equals и hashcode

--- src/main/java/backend/academy/labyrinths/entites/Cell.java
+++ src/main/java/backend/academy/labyrinths/entites/Cell.java
@@ -1,9 +1,9 @@
 package backend.academy.labyrinths.entites;
 
 import backend.academy.labyrinths.enums.CellType;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import java.util.Objects;
 
 /**
  * Класс клетки лабиринта
@@ -13,20 +13,38 @@ import lombok.Setter;
  */
 @Getter
 @Setter
-@EqualsAndHashCode
 public class Cell {
 
     private Coordinates coordinates;
 
     private CellType type;
 
+    private int cost;
+
     private boolean isVisited;
 
     public Cell(Coordinates coordinates, CellType type) {
         this.coordinates = coordinates;
         this.type = type;
+        this.cost = type.cost();
         this.isVisited = false;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cell cell = (Cell) o;
+        return this.isVisited == cell.isVisited && this.cost == cell.cost
+            && this.type.equals(cell.type)
+            && this.coordinates.x() == cell.coordinates.x() && this.coordinates.y() == cell.coordinates.y();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(coordinates, type);
+    }
+
 
     @Override
     public String toString() {
